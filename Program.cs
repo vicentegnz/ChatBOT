334d14ea7e1826de494f94d73eb1a72504aa5a09
vicente19600 <vicente19600@gmail.com>
2018-11-06 01:35:52 +0100

    Changes to Fix mode Debug

--- Program.cs
+++ Program.cs
@@ -8,35 +8,32 @@ namespace UniversityOfExtremaduraBOT
 {
     public class Program
     {
+      
         public static void Main(string[] args)
         {
+            BuildWebHost(args).Run();
+        }
 
-
-            var host = new WebHostBuilder()
-                .UseKestrel(options =>
-            {
-                options.Listen(IPAddress.Loopback, 44318, listenOptions =>
-                                listenOptions.UseHttps("localhost.pfx", "1234"));
-            })
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls("https://*:44318")
-                .UseIISIntegration()
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
 
-            host.Run();
-        }
 
 
-        //public static void Main(string[] args)
-        //{
-        //    BuildWebHost(args).Run();
-        //}
 
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
+        //public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args)
+        //        .UseKestrel(options =>
+        //    {
+        //        options.Listen(IPAddress.Loopback, 44318, listenOptions =>
+        //                        listenOptions.UseHttps("localhost.pfx", "1234"));
+        //    })
+        //        .UseContentRoot(Directory.GetCurrentDirectory())
+        //        .UseUrls("https://*:44318")
+        //        .UseIISIntegration()
         //        .UseStartup<Startup>()
         //        .Build();
+    
     }
 
 }
